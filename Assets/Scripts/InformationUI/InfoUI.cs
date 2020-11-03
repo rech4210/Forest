f5f4fdd46a448af653c85199fcad8a8d6254adda
MGT4869 <47241183+MGT4869@users.noreply.github.com>
2020-11-03 11:37:50 +0900

    기본적인 상태창 온오프 및 카메라 UI생성

---
+++ Assets/Scripts/InformationUI/InfoUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InfoUI_toggle()
+    {
+        if(this.gameObject.activeSelf == true)
+        {
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            this.gameObject.SetActive(true);
+        }
+    }
+}
