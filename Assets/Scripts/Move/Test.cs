59e50ad902fb575d38a615a0246454da9b659947
rech4210 <65288322+rech4210@users.noreply.github.com>
2020-11-10 15:51:56 +0900

    VR_Controller in/out put
    컨트롤러 인풋 받는 스크립트

---
+++ Assets/Scripts/Move/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using Valve.VR;
+
+public class Test : MonoBehaviour
+{
+    public SteamVR_Action_Boolean TriggerClick;
+    private SteamVR_Input_Sources inputSource;
+
+    private void Start() { }
+
+    private void OnEnable()
+    {
+        TriggerClick.AddOnStateDownListener(Press, inputSource);
+    }
+    private void OnDisable()
+    {
+        TriggerClick.RemoveOnStateDownListener(Press, inputSource);
+    }
+
+    private void Press(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        //put your stuff here
+        Debug.Log("Success!!");
+    }
+}
