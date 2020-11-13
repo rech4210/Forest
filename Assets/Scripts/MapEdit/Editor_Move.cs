de569c90c91744d7f73b835563018e76bc511805
wchang98 <47240838+wchang98@users.noreply.github.com>
2020-11-13 22:20:40 +0900

    에디터 조작 스크립트 추가
    wasd로 이동, 마우스 스크롤로 공중부양, Z로 되돌리기

---
+++ Assets/Scripts/MapEdit/Editor_Move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Editor_Move : MonoBehaviour
+{
+    public float speed = 10;
+    public float upDownPower = 10;
+
+    void Update()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+
+        if(xMove != 0 || zMove != 0)
+        {
+            Vector3 position = transform.position;
+            position.x += xMove * speed * Time.deltaTime;
+            position.z += zMove * speed * Time.deltaTime;
+            transform.position = position;
+        }
+
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            Vector3 position = transform.position;
+            position.y += upDownPower * Time.deltaTime;
+            transform.position = position;
+        }
+        else if(Input.mouseScrollDelta.y < 0)
+        {
+            Vector3 position = transform.position;
+            position.y -= upDownPower * Time.deltaTime;
+            transform.position = position;
+        }
+    }
+}
---
+++ Assets/Scripts/MapEdit/MapEditor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapEditor : MonoBehaviour
+{
+    public GameObject[] trees;
+
+    private int selectingIndex;
+    private GameObject map;
+    public List<GameObject> history;
+    private void Start()
+    {
+        map = new GameObject("Map");
+        history = new List<GameObject>();
+    }
+
+    void Update()
+    {
+        for(int i = (int)KeyCode.F1; i <= (int)KeyCode.F10; i++)
+        {
+            KeyCode keyCode = (KeyCode)i;
+            if (Input.GetKeyDown(keyCode))
+            {
+                selectingIndex = i - (int)KeyCode.F1;
+            }
+        }
+
+        if(Input.GetMouseButtonDown(0) || Input.GetMouseButton(1))
+        {
+            Create(transform.position);
+        }
+        else if(Input.GetKeyDown(KeyCode.Z))
+        {
+            Ctrl_Z();
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(10, 10, 100, 90), selectingIndex + "\n" + trees[selectingIndex].name);
+    }
+
+    private void Create(Vector3 position)
+    {
+        GameObject treeObject = Instantiate(trees[selectingIndex]);
+        treeObject.transform.position = transform.position;
+        history.Add(treeObject);
+        treeObject.transform.parent = map.transform;
+    }
+
+    private void Ctrl_Z()
+    {
+        Destroy(history[history.Count - 1]);
+        history.RemoveAt(history.Count - 1);
+    }
+}
