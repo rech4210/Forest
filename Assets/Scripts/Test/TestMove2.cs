70e31fc832ecf8bc52ccc8adbfe6de4e207f2cb2
anhyeongjong <65288275+anhyeongjong@users.noreply.github.com>
2020-11-13 19:03:43 +0900

    PlayerMoveController
    플레이어 이동 스크립트 완성

---
+++ Assets/Scripts/Test/TestMove2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMove2 : MonoBehaviour
+{
+    private CharacterController cc;
+    public Transform camTr;
+    public float speed = 1.5f;
+
+    void Start()
+    {
+        //camTr = Camera.main.GetComponent<Transform>();
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        cc.SimpleMove(camTr.forward * speed);
+    }
+}
