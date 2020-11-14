902878ce4fa13f25956dd1bf6f06ecb03159bd8a
wchang98 <47240838+wchang98@users.noreply.github.com>
2020-11-14 22:03:13 +0900

    Snow System 구현 (보완 필요)

---
+++ Assets/Scripts/Snow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snow : MonoBehaviour
+{
+    void Update()
+    {
+        if(transform.position.y < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SnowSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowSystem : MonoBehaviour
+{
+    public GameObject snowPrefab;
+    public GameObject player;
+    public float randomMinX, randomMaxX;
+    public float randomMinZ, randomMaxZ;
+    public float y;
+    public float dropInterval;
+    public float snowSpeed;
+
+    private void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.Find("Editor");
+        }
+
+        StartCoroutine(SnowProcess());
+    }
+
+    IEnumerator SnowProcess()
+    {
+        do
+        {
+            CreateSnow();
+            yield return new WaitForSecondsRealtime(dropInterval);
+        } while (true);
+    }
+
+    private void CreateSnow()
+    {
+        GameObject snow = Instantiate(snowPrefab);
+        float x = Random.Range(randomMinX, randomMaxX);
+        float z = Random.Range(randomMinZ, randomMaxZ);
+        snow.transform.parent = transform;
+        snow.transform.position = new Vector3(x, y, z) + player.transform.position;
+        snow.GetComponent<Rigidbody>().velocity = new Vector3(0, -snowSpeed, 0);
+    }
+}
